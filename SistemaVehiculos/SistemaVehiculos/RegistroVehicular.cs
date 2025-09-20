ee72cad36f45fb88fccc2db61ed8fd4cb625cbd7
abnertol2a-gif <abnertol2a@gmail.com>
2025-09-19 22:34:47 -0600

    Adding managing methods

---
+++ SistemaVehiculos/SistemaVehiculos/RegistroVehicular.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionVehiculos
+{
+    public class RegistroVehicular
+    {
+        private List<Vehiculo> vehiculos = new List<Vehiculo>();
+
+        public void AgregarVehiculo(Vehiculo v)
+        {
+            vehiculos.Add(v);
+        }
+
+        public void MostrarImpuestos()
+        {
+            Console.WriteLine("=== Impuestos de Vehículos ===");
+            foreach (var v in vehiculos)
+            {
+                Console.WriteLine($"{v} → Impuesto: Q{v.CalcularImpuesto():0.00}");
+            }
+        }
+
+        public void ListarVehiculos()
+        {
+            Console.WriteLine("=== Lista de Vehículos Registrados ===");
+            foreach (var v in vehiculos)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
