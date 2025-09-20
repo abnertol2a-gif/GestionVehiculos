2c1d684d10005f09b4b2069ae158b3a605bdbce7
abnertol2a-gif <abnertol2a@gmail.com>
2025-09-19 21:59:59 -0600

    Feat: Vehicle class file creation

---
+++ SistemaVehiculos/SistemaVehiculos/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GestionVehiculos
+{
+    public abstract class Vehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+
+        public Vehiculo(string marca, string modelo, int anio)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+        }
+
+        public abstract double CalcularImpuesto();
+
+        public override string ToString()
+        {
+            return $"{Marca} {Modelo} ({Anio})";
+        }
+    }
+}
